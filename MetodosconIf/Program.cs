1672e800a71a9222383c45032010e0ec4b63778a
Brayan Bastidas <brayanbmx_595@live.com>
2024-09-27 22:29:56 -0500

    Add files via upload

---
+++ MetodosconIf/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenido por favor indicanos tu nombre");
+        string name = Console.ReadLine();
+        Console.WriteLine($"{name}, presiona 1 para sumar o 2 para restar");
+        int decision = int.Parse( Console.ReadLine() );
+        if (decision == 1)
+        {
+            suma();
+        }
+        else resta();
+    }
+    // programa de metodo if y sobrecarga de metodos y/o metodos ------------------------------------------------------------
+    static void suma()
+    {
+        Console.WriteLine("Digite numero a sumar");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite su segundo numero a sumar");
+        int num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine($"su suma es {num1 + num2}");
+    }
+    static void resta()
+    {
+        Console.WriteLine("DIGITE SU NUMERO PARA RESTAR");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("DIGITE SU NUMERO PARA RESTAR");
+        int num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Su resta es {num1 - num2}");
+    }
+}
---
+++ Sobre carga de metodo/Program.cs
@@ -0,0 +1,54 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        sumaNumeros(2,3);
+        sumaNumeros(1, 2, 3);
+        sumaNumeros(1, 2, 3, 4);
+        sumaNumeros(2,2,2,3);
+        restaNumeros(2,2);
+        restaNumeros(8, 2, 1);
+        restaNumeros(1, 2, 3, 4); 
+    }
+    // Sobre carga de Metodos con Metodos sin Return -----------------------------------------------------------------------
+
+    static void sumaNumeros(int num1, int num2)
+    {
+        int resultado = num1 + num2;
+        Console.WriteLine("su suma es" + " " + resultado);
+    }
+    static void sumaNumeros(int num1, int num2, int num3)
+    {
+        int resultado = num1 + num2 + num3;
+        Console.WriteLine($"Su suma es {resultado}");
+    }
+    static void sumaNumeros(int num1, int num2, int num3, int num4)
+    {
+        int resultado = num1 + num2 + num3 + num4;
+        Console.WriteLine("su resultado es" + " " + resultado);
+    }
+    //-------------------------------------------------------------------------------------------------------------------------
+
+    // Sobre carga de Metodos con Metodos con Return---------------------------------------------------------------------------
+
+    static int restaNumeros(int num1, int num2)
+    {
+        int resultado = num1 - num2;
+        Console.WriteLine($"su resta es {resultado}");
+        return resultado;
+    }
+    static int restaNumeros(int num1, int num2, int num3)
+    {
+        int resultado = num1 - num2 - num3;
+        Console.WriteLine($"su resta es {resultado}");
+        return resultado;
+    }
+    static int restaNumeros(int num1,int num2,int num3,int num4)
+    {
+        int resultado = num1 - num2 - num3 - num4;
+        Console.WriteLine("su resultado es " + " " + resultado);
+        return resultado;
+    }
+
+    //------------------------------------------------------------------------------------------------------------------------
+}
